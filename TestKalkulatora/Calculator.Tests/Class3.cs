4a1013a87ab1c485cf84013f98d832dd6037ef37
Wojtek Grden <wojciech.grden95@gmail.com>
2018-05-08 13:40:39 +0200

    Dodanie nowych metod kaluklatora oraz odpowiednich testÃ³w do nich.

---
+++ TestKalkulatora/Calculator.Tests/Class3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Calculator.Tests;
+using Calculator;
+
+
+namespace Calculator.Tests
+{
+    public class Calculator_dtests
+    {
+        [Fact]
+        public void division_two_positive_integers()
+        {
+            // arange
+            var x = 10;
+            var y = 5;
+            var calc = new ExampleCalculator();
+            // act
+            var result = calc.division(x, y);
+            // assert
+            Assert.Equal(2, result);
+        }
+    }
+}
--- TestKalkulatora/TestKalkulatora/Program.cs
+++ TestKalkulatora/TestKalkulatora/Program.cs
@@ -17,7 +17,23 @@ namespace Calculator
             var calc = new ExampleCalculator();
             var result = calc.Add(x, y);
 
-            Console.WriteLine(result);
+            Console.WriteLine("Wynik dodawania dwóch zmiennych: {0}",result);
+
+            var result1 = calc.multiplication(x, y);
+
+            Console.WriteLine("Wynik mnożenia dwóch zmiennych: {0}", result1);
+
+            var result2 = calc.division(x, y);
+            var result3 = calc.reversedivision(x, y);
+
+            Console.WriteLine("Wynik dzielenia zmiennej x / y: {0} Wynik dzielenia zmiennej y / x: {1}", result2, result3);
+
+            var result4 = calc.subtraction(x, y);
+            var result5 = calc.reversesubtraction(x, y);
+
+            Console.WriteLine("Wynik odejmowania x - y: {0} Wynik odejmowania y - x: {1}", result4, result5);
+
+
         }
     }
 
@@ -30,6 +46,26 @@ namespace Calculator
         public int Add(int x, int y)
         {
             return x + y;
+        }
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+        public double division(double x, double y)
+        {
+            return x / y;
+        }
+        public double reversedivision(double x, double y)
+        {
+            return y / x;
+        }
+        public int subtraction(int x, int y)
+        {
+            return x - y;
+        }
+        public int reversesubtraction(int x, int y)
+        {
+            return y - x;
         }
     }
 }

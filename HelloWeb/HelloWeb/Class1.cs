9125fb59d481eba310471d0e5647b2019ec12b43
Wojtek Grden <wojciech.grden95@gmail.com>
2018-05-08 16:38:45 +0200

    Dodanie pierwszego testu strony

---
+++ HelloWeb/HelloWeb/Class1.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace HelloWeb
+{
+    public class HelloWebTests : IDisposable
+
+    {
+        private readonly string BlogUrl = "https://autotestdotnet.wordpress.com/";
+        private IWebDriver browser;
+        public HelloWebTests()
+        {
+            browser = new ChromeDriver();
+        }
+        [Fact]
+        public void Can_open_blog_and_hello_exists()
+        {
+            //open browser
+            browser.Navigate().GoToUrl(BlogUrl);
+            //open blog
+            var element = browser.FindElement(By.CssSelector("#post-3096>header>h1>a")).Text;
+            //check note exist
+            Assert.Equal("Witamy na warsztatach automatyzacji testów!", element);
+        }
+        //close
+        public void Dispose()
+        {
+            try
+            {
+                browser.Quit();
+            }
+            catch
+            {
+            }
+        }
+    }
+}

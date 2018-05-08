38dd5f29930b92715bd6ccc0bfb77f426b189412
Wojtek Grden <wojciech.grden95@gmail.com>
2018-05-08 10:11:19 +0200

    Przyklad"

---
+++ TestKalkulatora/TestKalkulatora/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKalkulatora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WItamy na kursie");
+            Console.WriteLine();
+        }
+    }
+}

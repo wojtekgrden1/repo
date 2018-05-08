de304ced8a793008531fe7542d7ca07b033972d3
Wojtek Grden <wojciech.grden95@gmail.com>
2018-05-08 13:44:33 +0200

    Poprawe istniejacych testÃw.

--- TestKalkulatora/Calculator.Tests/Class1.cs
+++ TestKalkulatora/Calculator.Tests/Class1.cs
@@ -24,5 +24,41 @@ namespace Calculator.Tests
             // assert
             Assert.Equal(15, result);
         }
+        [Fact]
+        public void multiplication_two_positive_integers()
+        {
+            // arange
+            var x = 5;
+            var y = 10;
+            var calc = new ExampleCalculator();
+            // act
+            var result = calc.multiplication(x, y);
+            // assert
+            Assert.Equal(50, result);
+        }
+        [Fact]
+        public void division_two_positive_integers()
+        {
+            // arange
+            var x = 10;
+            var y = 5;
+            var calc = new ExampleCalculator();
+            // act
+            var result = calc.division(x, y);
+            // assert
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public void subtraction_two_positive_integers()
+        {
+            // arange
+            var x = 5;
+            var y = 10;
+            var calc = new ExampleCalculator();
+            // act
+            var result = calc.subtraction(x, y);
+            // assert
+            Assert.Equal(-5, result);
+        }
     }
 }
